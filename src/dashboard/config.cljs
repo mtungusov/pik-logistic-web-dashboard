9c02d1853acebeb35a46f05ca2da55f31086be89
TM <tm@macadmin.ru>
2017-11-08 17:14:22 +0300

    change. prod mode

--- src/dashboard/config.cljs
+++ src/dashboard/config.cljs
@@ -1,4 +1,5 @@
 (ns dashboard.config)
 
-(def api-host "http://localhost:3000/api/v2")
+; (def api-host "http://localhost:3000/api/v2")
 ; (def api-host "http://192.168.225.234:3000/api/v2")
+(def api-host "https:/dashboard-cars.pik-industry.ru/api/v3")
